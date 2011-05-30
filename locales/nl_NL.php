671be18d7f60cb9bc17e5ccf13f43f62bfe663fe
remi <remi@63ed0bee-1e5a-40ed-97f3-be6438125e5f>
2011-05-30 08:55:04 +0000

    Dutch locale, thanks Sander Middelhof, see #2822
    git-svn-id: https://forge.glpi-project.org/svn/behaviors/trunk@26 63ed0bee-1e5a-40ed-97f3-be6438125e5f

---
+++ locales/nl_NL.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2010 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+ */
+
+// Dutch translation by Sander Middelhof
+
+$LANG['plugin_behaviors'][0] = "Behaviours";
+
+// Config
+$LANG['plugin_behaviors'][1] = "Gebruik de aan een item toegewezen groep";
+$LANG['plugin_behaviors'][2] = "Gebruik de aanvragersgroep";
+$LANG['plugin_behaviors'][3] = "SQL filter voor aanvragersgroep";
+$LANG['plugin_behaviors'][4] = "SQL filter voor helpdeskmedewerkersgroep";
+$LANG['plugin_behaviors'][5] = "Verkeerde SQL filter";
+$LANG['plugin_behaviors'][6] = "Gebruik de helpdeskmedewerkersgroep";
+$LANG['plugin_behaviors'][7] = "Tijdsduur is verplciht voor een ticket is opgelost/gesloten";
+$LANG['plugin_behaviors'][8] = "Type oplossing is verplicht voor een ticket is opgelost/gesloten";
+$LANG['plugin_behaviors'][9] = "Zet de financiele startdatum als de status is";
+$LANG['plugin_behaviors'][10] = "Ticketnummer formaat";
+$LANG['plugin_behaviors'][11] = "Verwijder computers in OCS als ze uit GLPI verwijderd worden";
+$LANG['plugin_behaviors'][12] = "Plugin \"Item's uninstallation\" niet ge�nstalleerd";
+$LANG['plugin_behaviors'][13] = "Aanvrager is verplicht";
+$LANG['plugin_behaviors'][14] = "Deny change of ticket's creation date";
+
+// Message
+$LANG['plugin_behaviors'][100] = "Kan ticket niet sluiten zonder oplossingstype";
+$LANG['plugin_behaviors'][101] = "Kan ticket niet sluiten zonder opgegeven tijdsduur.";
+?>
