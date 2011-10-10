b1f32ea87f5b3da6879b75d31e909a3b5a6d20d4
remi <remi@63ed0bee-1e5a-40ed-97f3-be6438125e5f>
2011-10-10 13:33:21 +0000

    relicensed under AGPLv3+, fixed #3190
    git-svn-id: https://forge.glpi-project.org/svn/behaviors/trunk@69 63ed0bee-1e5a-40ed-97f3-be6438125e5f

--- inc/group_ticket.class.php
+++ inc/group_ticket.class.php
@@ -1,30 +1,34 @@
 <?php
-/*
+/**
  * @version $Id$
- -------------------------------------------------------------------------
- GLPI - Gestionnaire Libre de Parc Informatique
- Copyright (C) 2003-2011 by the INDEPNET Development Team.
-
- http://indepnet.net/   http://glpi-project.org
  -------------------------------------------------------------------------
 
  LICENSE
 
- This file is part of GLPI.
+ This file is part of Behaviors plugin for GLPI.
 
- GLPI is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
+ Behaviors is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- GLPI is distributed in the hope that it will be useful,
+ Behaviors is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with GLPI; if not, write to the Free Software Foundation, Inc.,
- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ You should have received a copy of the GNU Affero General Public License
+ along with Behaviors. If not, see <http://www.gnu.org/licenses/>.
+
+ @package   behaviors
+ @author    Remi Collet
+ @copyright Copyright (c) 2010-2011 Behaviors plugin team
+ @license   AGPL License 3.0 or (at your option) any later version
+            http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ @link      https://forge.indepnet.net/projects/behaviors
+ @link      http://www.glpi-project.org/
+ @since     2011
+
  --------------------------------------------------------------------------
 */
 
