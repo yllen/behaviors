fdd83c08902deacf3627e6186fa47d87cf4424c2
remi <remi@63ed0bee-1e5a-40ed-97f3-be6438125e5f>
2011-11-22 14:28:00 +0000

    fix tab name + license
    git-svn-id: https://forge.glpi-project.org/svn/behaviors/trunk@70 63ed0bee-1e5a-40ed-97f3-be6438125e5f

--- setup.php
+++ setup.php
@@ -85,6 +85,7 @@ function plugin_version_behaviors() {
 
    return array('name'           => $LANG['plugin_behaviors'][0],
                 'version'        => '0.83.0',
+                'license'        => 'AGPLv3+',
                 'author'         => 'Remi Collet',
                 'homepage'       => 'https://forge.indepnet.net/projects/behaviors',
                 'minGlpiVersion' => '0.83');// For compatibility / no install in version < 0.72
