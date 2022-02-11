60134c42e23c80bb00ad145af6dce104370852ff
yllen <nini.lasson@orange.fr>
2022-02-11 15:44:08 +0100

    update header

--- inc/problem.class.php
+++ inc/problem.class.php
@@ -22,7 +22,7 @@
 
  @package   behaviors
  @author    Nelly Mahu-Lasson
- @copyright Copyright (c) 2010-2021 Behaviors plugin team
+ @copyright Copyright (c) 2010-2022 Behaviors plugin team
  @license   AGPL License 3.0 or (at your option) any later version
             http://www.gnu.org/licenses/agpl-3.0-standalone.html
  @link      https://forge.glpi-project.org/projects/behaviors
